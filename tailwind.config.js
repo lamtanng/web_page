ce748b018c7be4ce8297dec4a32b6e7f48417fc0
lamtanng <laam122003@gmail.com>
2024-06-10 16:44:14 +0700

    style: create base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,66 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+const themeColors = {
+  primary: {
+    1: "rgba(0, 18, 164, 0.1)",
+    2: "rgba(0, 168, 164, 0.2)",
+    3: "rgba(0, 168, 164, 0.4)",
+    4: "rgba(0, 168, 164, 0.6)",
+    5: "rgba(0, 168, 164, 1)",
+    6: "#008790",
+    7: "#006678",
+  },
+  gray: {
+    1: "#f4f4f6",
+    2: "#e9e9ed",
+    3: "#dddde3",
+    4: "#d2d2da",
+    5: "#c7c7d1",
+    6: "#bcbcc8",
+    8: "#a5a5b6",
+    12: "#71738b",
+    22: "#0d0f35",
+  },
+};
+
+const theme = {
+  bgColors: {
+    primary: colors.white,
+    secondary: themeColors.gray[1],
+    tertiary: themeColors.gray[22],
+  },
+
+  textColors: {
+    primary: themeColors.gray[22],
+    secondary: themeColors.gray[12],
+    tertiary: themeColors.gray[6],
+    disable: themeColors.gray[6],
+  },
+  colors: {
+    main: themeColors.primary[5],
+    light: themeColors.gray[1],
+    dark: themeColors.gray[7],
+  },
+};
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...theme.colors,
+      },
+      textColor: {
+        ...theme.textColors,
+      },
+      backgroundColor: {
+        ...theme.bgColors,
+      },
+      fontFamily: {
+        centra: ["CentraNo2", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
